69e065276d68a41bf89a99a09eb40b1c14099f19
deraugla <deraugla>
2013-03-30 01:16:33 +0000

    -

--- ocaml/poly_tree.mli
+++ ocaml/poly_tree.mli
@@ -1,4 +1,4 @@
-(* $Id: poly_tree.mli,v 1.13 2013-03-30 01:13:40 deraugla Exp $ *)
+(* $Id: poly_tree.mli,v 1.14 2013-03-30 01:16:32 deraugla Exp $ *)
 
 open Pnums;
 open Field;
@@ -32,7 +32,7 @@ value without_initial_neg : field α → tree α → option (tree α);
 value substitute_y : field α → tree α → tree α → tree α;
 
 value tree_pow_list_y : field α → tree α → polynomial (tree α) int;
-value const_pow_list_x : field α → tree α → list (monomial α Q.t);
+value const_pow_list_x : field α → tree α → polynomial α Q.t;
 
 value group : field α → tree α → list (monomial (list (monomial α Q.t)) int);
 value normalize : field α → tree α → tree α;
