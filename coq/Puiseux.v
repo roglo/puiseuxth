81e5cb878ca4285a5e01c817dae8f3015f540e09
deraugla <deraugla>
2013-04-24 19:35:08 +0000

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -1,4 +1,4 @@
-(* $Id: Puiseux.v,v 1.308 2013-04-24 18:27:03 deraugla Exp $ *)
+(* $Id: Puiseux.v,v 1.309 2013-04-24 19:35:08 deraugla Exp $ *)
 
 (* Most of notations are Robert Walker's ones *)
 
@@ -960,6 +960,7 @@ rename H into Hep₁.
 rewrite Hep₁ in Hnp.
 destruct Hhps as [Hhps| Hhps].
  injection Hhps; clear Hhps; intros; subst h hps.
+(**)
  eapply lt_trans in Hkh; [ idtac | eassumption ].
  apply lt_irrefl in Hkh; contradiction.
 
@@ -1053,6 +1054,50 @@ unfold fst_fst_lt in Hlt; simpl in Hlt.
 
 unfold lower_convex_hull_points in Heqlch.
 remember (length pts) as n; clear Heqn.
+rename Heqlch into Hnp.
+rename Hpts₂ into Hsort.
+destruct n; [ discriminate Hnp | idtac ].
+destruct pts as [| pt₁]; [ discriminate Hnp | idtac ].
+remember Hnp as H; clear HeqH.
+apply next_ch_points_hd in H.
+subst pt₁; simpl in Hnp.
+destruct pts as [| pt₁]; [ discriminate Hnp | idtac ].
+remember (minimise_slope α (j, jps) pt₁ pts) as ms₁.
+injection Hnp; clear Hnp; intros; subst segjk.
+remember H as Hnp; clear HeqHnp.
+apply next_ch_points_hd in H.
+rename H into Hep₁.
+rewrite Hep₁ in Hnp.
+destruct Hhps as [Hhps| Hhps].
+ injection Hhps; clear Hhps; intros; subst h hps.
+ apply lt_irrefl in Hjh; contradiction.
+
+ destruct Hhps as [Hhps| Hhps]; [ exfalso | idtac ].
+  subst pt₁.
+  symmetry in Heqms₁.
+  apply LocallySorted_inv in Hsort; destruct Hsort as (Hlt₁, Hsort).
+  apply minimise_slope_le in Heqms₁; [ idtac | assumption ].
+  rewrite Hep₁ in Heqms₁.
+bbb.
+
+  Focus 2.
+  symmetry in Heqms₁.
+  symmetry in Hep₁.
+  remember Heqms₁ as H; clear HeqH.
+  eapply minimised_slope in H; [ idtac | eassumption ].
+  symmetry in Hep₁.
+bbb.
+  eapply minimise_slope_lt in Heqms₁; try eassumption.
+   rewrite H in Heqms₁.
+   subst β γ.
+   apply ad_hoc_lt_lt; [ idtac | assumption ].
+   split; assumption.
+
+   apply LocallySorted_inv in Hsort; destruct Hsort as (Hlt₁, Hsort).
+   destruct pts as [| pt₂]; [ constructor | idtac ].
+   apply LocallySorted_inv in Hsort; destruct Hsort; assumption.
+(**)
+bbb.
 destruct n; [ discriminate Heqlch | simpl in Heqlch ].
 destruct pts as [| (l, lps)]; [ discriminate Heqlch | idtac ].
 destruct pts as [| (m, mps)]; [ discriminate Heqlch | idtac ].
@@ -1065,9 +1110,9 @@ destruct Hhps as [Hhps| Hhps].
  injection Hhps; clear Hhps; intros; subst h hps.
  apply lt_irrefl in Hjh; contradiction.
 
+(*
  destruct Hhps as [Hhps| Hhps].
   injection Hhps; clear Hhps; intros; subst m mps.
-(*
   apply minimise_slope_pts_le with (h := h) (hps := hps) in Heqms₂.
 *)
 bbb.
