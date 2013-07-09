c169bfc58bbeb5f8c4275d9ddaa12352a8bbcd3a
deraugla <deraugla>
2013-07-09 18:59:53 +0000

    -

--- coq/Fpolynomial.v
+++ coq/Fpolynomial.v
@@ -1,4 +1,4 @@
-(* $Id: Fpolynomial.v,v 1.10 2013-07-09 16:52:27 deraugla Exp $ *)
+(* $Id: Fpolynomial.v,v 1.11 2013-07-09 18:59:53 deraugla Exp $ *)
 
 (* polynomials on a field *)
 
@@ -309,8 +309,31 @@ Lemma pol_add_loop_an_assoc :
     → fld_eq fld (an rp₁) (an rp₂) = true.
 Proof.
 intros α fld an₁ an₂ an₃ al₁ al₂ al₃ rp₁ rp₂ H₁ H₂.
+revert an₁ an₂ an₃ al₂ al₃ rp₁ rp₂ H₁ H₂.
+induction al₁ as [| a₁]; intros.
+ simpl in H₁, H₂.
+
+bbb.
+intros α fld an₁ an₂ an₃ al₁ al₂ al₃ rp₁ rp₂ H₁ H₂.
+revert an₁ an₂ an₃ al₁ al₃ rp₁ rp₂ H₁ H₂.
+induction al₂ as [| a₂]; intros.
+ simpl in H₂.
+ destruct al₃ as [| a₃]; simpl in H₁, H₂.
+  eapply pol_add_loop_al_comm in H₂; [ idtac | reflexivity ].
+  simpl in H₂.
+  destruct al₁ as [| a₁]; simpl in H₁, H₂.
+   subst rp₁; simpl.
+   remember (al rp₂) as al₂.
+   destruct al₂; [ idtac | discriminate H₂ ].
+   Focus 2.
+   subst rp₁; simpl.
+   remember (al rp₂) as al₂.
+   destruct al₂ as [| a₂]; [ discriminate H₂ | idtac ].
+   apply andb_true_iff in H₂.
+   destruct H₂ as (H₁, H₂).
 bbb.
 *)
+
 
 Lemma poly_add_assoc : ∀ α (fld : field α) pol₁ pol₂ pol₃,
   poly_eq fld
