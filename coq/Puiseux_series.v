8cca2d799dad3db815e8884872752c915c1a4e8a
deraugla <deraugla>
2013-08-07 14:25:11 +0000

    -

--- coq/Puiseux_series.v
+++ coq/Puiseux_series.v
@@ -1,4 +1,4 @@
-(* $Id: Puiseux_series.v,v 1.135 2013-08-07 14:06:28 deraugla Exp $ *)
+(* $Id: Puiseux_series.v,v 1.136 2013-08-07 14:25:11 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import QArith.
@@ -620,6 +620,10 @@ Qed.
 Lemma Pos_divide_lcm_l : ∀ a b, (a | Plcm a b)%positive.
 Proof.
 intros a b.
+unfold Plcm, Z.lcm, Pos.divide.
+rewrite Z.lcm_equiv1; simpl; [ idtac | apply Zpos_ne_0 ].
+exists (Z.to_pos (Z.abs (' b / ' Pos.gcd a b))).
+symmetry; rewrite Pos.mul_comm.
 bbb.
 *)
 
