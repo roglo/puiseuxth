78c5766a65646ff787d927a6fe134732bd75fc76
deraugla <deraugla>
2013-11-20 14:06:02 +0000

    -

--- coq/Field.v
+++ coq/Field.v
@@ -1,4 +1,4 @@
-(* $Id: Field.v,v 2.9 2013-11-20 12:56:33 deraugla Exp $ *)
+(* $Id: Field.v,v 2.10 2013-11-20 14:06:02 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import Setoid.
@@ -10,7 +10,7 @@ Record field α :=
     one : α;
     add : α → α → α;
     mul : α → α → α;
-    neg : α → α;
+    opp : α → α;
     fld_eq : α → α → Prop;
     fld_eq_refl : ∀ a, fld_eq a a;
     fld_eq_sym : ∀ a b, fld_eq a b → fld_eq b a;
@@ -18,14 +18,14 @@ Record field α :=
     fld_add_comm : ∀ a b, fld_eq (add a b) (add b a);
     fld_add_assoc : ∀ a b c, fld_eq (add a (add b c)) (add (add a b) c);
     fld_add_0_l : ∀ a, fld_eq (add zero a) a;
-    fld_add_neg : ∀ a, fld_eq (add a (neg a)) zero;
+    fld_add_opp_diag_r : ∀ a, fld_eq (add a (opp a)) zero;
     fld_add_compat_r : ∀ a b c, fld_eq a b → fld_eq (add a c) (add b c);
     fld_mul_comm : ∀ a b, fld_eq (mul a b) (mul b a);
     fld_mul_assoc : ∀ a b c, fld_eq (mul a (mul b c)) (mul (mul a b) c);
     fld_mul_1_l : ∀ a, fld_eq (mul one a) a;
     fld_mul_compat_r : ∀ a b c, fld_eq a b → fld_eq (mul a c) (mul b c);
     fld_mul_add_distr_l : ∀ a b c,
-      mul a (add b c) = add (mul a b) (mul a c) }.
+      fld_eq (mul a (add b c)) (add (mul a b) (mul a c)) }.
 
 Add Parametric Relation α (fld : field α) : α (fld_eq fld)
  reflexivity proved by (fld_eq_refl fld)
@@ -63,17 +63,28 @@ rewrite fld_add_comm.
 apply fld_add_0_l.
 Qed.
 
-Theorem neg_zero : ∀ α (fld : field α),
-  fld_eq fld (neg fld (zero fld)) (zero fld).
+Theorem fld_opp_0 : ∀ α (fld : field α),
+  fld_eq fld (opp fld (zero fld)) (zero fld).
 Proof.
 intros α fld.
+etransitivity; [ idtac | apply fld_add_opp_diag_r ].
+symmetry; apply fld_add_0_l.
+Qed.
+
+Theorem fld_mul_opp_l : ∀ α (fld : field α) a b,
+  fld_eq fld (mul fld (opp fld a) b) (opp fld (mul fld a b)).
+Proof.
 bbb.
 
 Theorem fld_mul_0_l : ∀ α (fld : field α) a,
-  fld_eq fld (mul fld (zero fld) a) a.
+  fld_eq fld (mul fld (zero fld) a) (zero fld).
 Proof.
 intros α fld a.
-replace (zero fld) with (add fld (zero fld) (neg fld (zero fld))) .
+assert (fld_eq fld (zero fld) (add fld (zero fld) (opp fld (zero fld)))).
+ symmetry.
+ apply fld_add_opp_diag_r.
+
+ rewrite H in |- * at 1.
  rewrite fld_mul_comm.
  rewrite fld_mul_add_distr_l.
 bbb.
