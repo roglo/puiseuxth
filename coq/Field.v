0cdb0f33e811b80b9b690687feedcf903c2cbfbc
deraugla <deraugla>
2013-11-20 20:13:06 +0000

    -

--- coq/Field.v
+++ coq/Field.v
@@ -1,7 +1,8 @@
-(* $Id: Field.v,v 2.12 2013-11-20 19:49:55 deraugla Exp $ *)
+(* $Id: Field.v,v 2.13 2013-11-20 20:13:06 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import Ring_theory.
+Require Import Field_theory.
 Require Import Setoid.
 
 Set Implicit Arguments.
@@ -12,10 +13,12 @@ Record field α :=
     add : α → α → α;
     mul : α → α → α;
     opp : α → α;
+    inv : α → α;
     fld_eq : α → α → Prop;
     fld_eq_refl : ∀ a, fld_eq a a;
     fld_eq_sym : ∀ a b, fld_eq a b → fld_eq b a;
     fld_eq_trans : ∀ a b c, fld_eq a b → fld_eq b c → fld_eq a c;
+    fld_1_neq_0 : not (fld_eq one zero);
     fld_add_comm : ∀ a b, fld_eq (add a b) (add b a);
     fld_add_assoc : ∀ a b c, fld_eq (add a (add b c)) (add (add a b) c);
     fld_add_0_l : ∀ a, fld_eq (add zero a) a;
@@ -25,6 +28,7 @@ Record field α :=
     fld_mul_assoc : ∀ a b c, fld_eq (mul a (mul b c)) (mul (mul a b) c);
     fld_mul_1_l : ∀ a, fld_eq (mul one a) a;
     fld_mul_compat_r : ∀ a b c, fld_eq a b → fld_eq (mul a c) (mul b c);
+    fld_mul_inv : ∀ a, not (fld_eq a zero) → fld_eq (mul (inv a) a) one;
     fld_mul_add_distr_l : ∀ a b c,
       fld_eq (mul a (add b c)) (add (mul a b) (mul a c)) }.
 
@@ -56,30 +60,6 @@ rewrite fld_mul_comm; symmetry.
 rewrite fld_mul_compat_r; [ reflexivity | eassumption ].
 Qed.
 
-Lemma fld_mul_add_distr_r : ∀ α (fld : field α) x y z,
- fld_eq fld (mul fld (add fld x y) z) (add fld (mul fld x z) (mul fld y z)).
-Proof.
-intros a fld x y z.
-rewrite fld_mul_comm.
-rewrite fld_mul_add_distr_l.
-rewrite fld_mul_comm.
-assert (fld_eq fld (mul fld z y) (mul fld y z)) as H.
- apply fld_mul_comm.
-
- rewrite H; reflexivity.
-Qed.
-
-Definition machin α (fld : field α) :=
-  {| Radd_0_l := fld_add_0_l fld;
-     Radd_comm := fld_add_comm fld;
-     Radd_assoc := fld_add_assoc fld;
-     Rmul_1_l := fld_mul_1_l fld;
-     Rmul_comm := fld_mul_comm fld;
-     Rmul_assoc := fld_mul_assoc fld;
-     Rdistr_l := fld_mul_add_distr_r fld;
-     Rsub_def x y := fld_eq_refl fld (add fld x (opp fld y));
-     Ropp_def := fld_add_opp_diag_r fld |}.
-
 Section fld.
 
 Variable α : Type.
@@ -95,6 +75,40 @@ Notation "a - b" := (add fld a (opp fld b))
   (left associativity, at level 50) : fld_scope.
 Notation "a * b" := (mul fld a b)
   (left associativity, at level 40) : fld_scope.
+
+Theorem fld_mul_add_distr_r : ∀ x y z,
+ ((x + y) * z)%fld ≍ (x * z + y * z)%fld.
+Proof.
+intros x y z.
+rewrite fld_mul_comm.
+rewrite fld_mul_add_distr_l.
+rewrite fld_mul_comm.
+assert (fld_eq fld (mul fld z y) (mul fld y z)) as H.
+ apply fld_mul_comm.
+
+ rewrite H; reflexivity.
+Qed.
+
+(* oui, bon, mais qu'est-ce qu'on en fait, de ces trucs-là ? *)
+
+Definition my_ring :=
+  {| Radd_0_l := fld_add_0_l fld;
+     Radd_comm := fld_add_comm fld;
+     Radd_assoc := fld_add_assoc fld;
+     Rmul_1_l := fld_mul_1_l fld;
+     Rmul_comm := fld_mul_comm fld;
+     Rmul_assoc := fld_mul_assoc fld;
+     Rdistr_l := fld_mul_add_distr_r;
+     Rsub_def x y := fld_eq_refl fld (add fld x (opp fld y));
+     Ropp_def := fld_add_opp_diag_r fld |}.
+
+Definition my_field :=
+  {| F_R := my_ring;
+     F_1_neq_0 := fld_1_neq_0 fld;
+     Fdiv_def x y := fld_eq_refl fld (mul fld x (inv fld y));
+     Finv_l := fld_mul_inv fld |}.
+
+(* *)
 
 Theorem fld_add_0_r : ∀ a, (a + 0 ≍ a)%fld.
 Proof.
