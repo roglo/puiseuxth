e2a9765e8e2ba0a81b59d5f701671b1ed5247569
deraugla <deraugla>
2013-11-24 20:42:14 +0000

    -

--- coq/Series.v
+++ coq/Series.v
@@ -1,4 +1,4 @@
-(* $Id: Series.v,v 2.39 2013-11-24 20:37:38 deraugla Exp $ *)
+(* $Id: Series.v,v 2.40 2013-11-24 20:42:14 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import QArith.
@@ -811,6 +811,12 @@ rewrite <- series_inf_eq.
 reflexivity.
 Qed.
 
+Lemma zzz : ∀ a b i,
+  series_nth_fld fld i (series_mul_inf a b)
+  ≍ terms (series_mul_inf a b) i.
+Proof.
+bbb.
+
 Theorem series_mul_assoc : ∀ a b c,
   series_mul fld a (series_mul fld b c)
   ≃ series_mul fld (series_mul fld a b) c.
@@ -827,9 +833,7 @@ remember (series_inf fld a) as aa eqn:Haa .
 remember (series_inf fld b) as bb eqn:Hbb .
 remember (series_inf fld c) as cc eqn:Hcc .
 constructor; intros k.
-unfold series_nth_fld; simpl.
-destruct (Nbar.lt_dec (fin k) ∞) as [H₁| H₁]; [ idtac | reflexivity ].
-clear H₁.
+do 2 rewrite zzz; simpl.
 unfold convol_mul_inf; simpl.
 bbb.
 
