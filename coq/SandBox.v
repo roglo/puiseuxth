fa763d5c8f2a0e01150a0f5cde83e92622c11872
deraugla <deraugla>
2013-12-02 10:44:45 +0000

    -

--- coq/SandBox.v
+++ coq/SandBox.v
@@ -1,4 +1,4 @@
-(* $Id: SandBox.v,v 2.151 2013-12-01 17:04:02 deraugla Exp $ *)
+(* $Id: SandBox.v,v 2.152 2013-12-02 10:44:45 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import QArith.
@@ -215,22 +215,21 @@ Qed.
 
 Lemma sigma_add : ∀ f k₁ k₂,
   (0 < k₁)%nat
-  → (0 < k₂)%nat
-    → Σ (i = 0, k₁ + k₂ - 1)   f i
-      ≍ (Σ (i = 0, k₁ - 1)   f i + Σ (i = k₁, k₁ + k₂ - 1)   f i)%fld.
+  → Σ (i = 0, k₁ + k₂)   f i
+    ≍ (Σ (i = 0, k₁ - 1)   f i + Σ (i = k₁, k₁ + k₂)   f i)%fld.
 Proof.
-intros f k₁ k₂ Hk₁ Hk₂.
+intros f k₁ k₂ Hk₁.
 unfold sigma.
 do 2 rewrite Nat.sub_0_r.
 rewrite <- Nat.sub_succ_l.
  rewrite Nat.sub_succ, Nat.sub_0_r.
- rewrite sigma_aux_add.
- rewrite <- Nat.sub_succ_l; [ idtac | assumption ].
- rewrite Nat.sub_succ, Nat.sub_0_r.
- rewrite Nat.add_0_l, Nat.add_comm.
- rewrite Nat.add_sub; reflexivity.
+ rewrite <- Nat.add_succ_r.
+ rewrite sigma_aux_add; simpl.
+ rewrite Nat.add_comm, Nat.add_sub.
+ reflexivity.
 
- omega.
+ destruct k₁; [ exfalso; revert Hk₁; apply Nat.lt_irrefl | idtac ].
+ apply le_n_S, Nat.le_0_l.
 Qed.
 
 Lemma sigma_aux_succ : ∀ f b k,
@@ -349,25 +348,20 @@ bbb.
 
 (**)
 Lemma inserted_0_sigma : ∀ f g k n,
-  (∀ i, i mod n ≠ O → f i ≍ 0%fld)
-  → (∀ i, f (n * i)%nat ≍ g i)
-    → Σ (i = 0, k * n)   f i ≍ Σ (i = 0, k)   g i.
+  n ≠ O
+  → (∀ i, i mod n ≠ O → f i ≍ 0%fld)
+    → (∀ i, f (n * i)%nat ≍ g i)
+      → Σ (i = 0, k * n)   f i ≍ Σ (i = 0, k)   g i.
 Proof.
-intros f g k n Hf Hfg.
-replace (k * n)%nat with (k * n + 1 - 1)%nat .
- rewrite sigma_add.
-  rewrite sigma_mul_sigma_sigma.
-bbb.
-n'importe quoi...
+intros f g k n Hn Hf Hfg.
+destruct k.
+ rewrite Nat.mul_0_l.
+ apply sigma_compat; intros i (_, Hi).
+ apply Nat.le_0_r in Hi; subst i.
+ rewrite <- Hfg, Nat.mul_0_r; reflexivity.
 
-intros f g k n Hf Hfg.
-apply inserted_0_sigma_aux.
- intros i Hi; simpl.
- apply Hf; assumption.
-
- intros i; simpl.
- apply Hfg.
-qed.
+ destruct n; [ exfalso; apply Hn; reflexivity | clear Hn ].
+ replace (S k * S n)%nat with (S k * S n - 1 + 1)%nat .
 *)
 
 Lemma zzz : ∀ a b k,
