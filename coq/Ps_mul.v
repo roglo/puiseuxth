16596a9bb5463feced5f6fcf86c13ee3e5f63996
deraugla <deraugla>
2013-12-08 03:09:45 +0000

    -

--- coq/Ps_mul.v
+++ coq/Ps_mul.v
@@ -1,4 +1,4 @@
-(* $Id: Ps_mul.v,v 2.38 2013-12-08 03:04:32 deraugla Exp $ *)
+(* $Id: Ps_mul.v,v 2.39 2013-12-08 03:09:45 deraugla Exp $ *)
 
 Require Import Utf8.
 Require Import QArith.
@@ -27,13 +27,13 @@ Definition ps_mul ps₁ ps₂ :=
 
 Notation "a * b" := (ps_mul a b) : ps_scope.
 
-Lemma nz_norm_mul_comm : ∀ nz₁ nz₂,
-  normalise_ps (ps_mul nz₁ nz₂) ≐ normalise_ps (ps_mul nz₂ nz₁).
+Lemma ps_norm_mul_comm : ∀ ps₁ ps₂,
+  normalise_ps (ps_mul ps₁ ps₂) ≐ normalise_ps (ps_mul ps₂ ps₁).
 Proof.
-intros nz₁ nz₂.
+intros ps₁ ps₂.
 unfold normalise_ps; simpl.
-remember (series_stretch (cm_factor nz₁ nz₂) (ps_terms nz₁)) as s₁ eqn:Hs₁ .
-remember (series_stretch (cm_factor nz₂ nz₁) (ps_terms nz₂)) as s₂ eqn:Hs₂ .
+remember (series_stretch (cm_factor ps₁ ps₂) (ps_terms ps₁)) as s₁ eqn:Hs₁ .
+remember (series_stretch (cm_factor ps₂ ps₁) (ps_terms ps₂)) as s₂ eqn:Hs₂ .
 rewrite series_mul_comm.
 remember (null_coeff_range_length rng (series_mul s₂ s₁) 0) as n eqn:Hn .
 destruct n as [n| ]; [ idtac | reflexivity ].
@@ -52,8 +52,8 @@ constructor; simpl.
 
  unfold gcd_ps; simpl.
  unfold cm; rewrite Pos.mul_comm, series_mul_comm.
- remember (ps_valnum nz₁ * ' ps_comden nz₂)%Z as x eqn:Hx .
- remember (ps_valnum nz₂ * ' ps_comden nz₁)%Z as y eqn:Hy .
+ remember (ps_valnum ps₁ * ' ps_comden ps₂)%Z as x eqn:Hx .
+ remember (ps_valnum ps₂ * ' ps_comden ps₁)%Z as y eqn:Hy .
  replace (x + y)%Z with (y + x)%Z by apply Z.add_comm.
  reflexivity.
 Qed.
@@ -62,7 +62,7 @@ Theorem ps_mul_comm : ∀ ps₁ ps₂, (ps₁ * ps₂ = ps₂ * ps₁)%ps.
 Proof.
 intros ps₁ ps₂.
 constructor.
-apply nz_norm_mul_comm.
+apply ps_norm_mul_comm.
 Qed.
 
 Lemma fold_series_1 : {| terms := λ _, 1%rng; stop := 1 |} = 1%ser.
@@ -150,7 +150,6 @@ Proof. intros ps. rewrite ps_mul_comm. apply ps_mul_1_l. Qed.
 Theorem ps_mul_0_l : ∀ ps, (0 * ps = 0)%ps.
 Proof.
 intros ps.
-destruct ps as (nz); simpl.
 constructor.
 unfold normalise_ps; simpl.
 unfold cm_factor; simpl.
@@ -544,11 +543,11 @@ constructor; simpl.
  reflexivity.
 Qed.
 
-Lemma eq_norm_ps_mul_compat_r : ∀ nz₁ nz₂ nz₃,
-  eq_norm_ps nz₁ nz₂
-  → eq_norm_ps (ps_mul nz₁ nz₃) (ps_mul nz₂ nz₃).
+Lemma eq_norm_ps_mul_compat_r : ∀ ps₁ ps₂ ps₃,
+  eq_norm_ps ps₁ ps₂
+  → eq_norm_ps (ps_mul ps₁ ps₃) (ps_mul ps₂ ps₃).
 Proof.
-intros nz₁ nz₂ nz₃ Heq.
+intros ps₁ ps₂ ps₃ Heq.
 induction Heq.
 constructor; simpl.
  rewrite H, H0; reflexivity.
@@ -561,11 +560,11 @@ constructor; simpl.
  reflexivity.
 Qed.
 
-Lemma eq_norm_ps_mul_compat_l : ∀ nz₁ nz₂ nz₃,
-  eq_norm_ps nz₁ nz₂
-  → eq_norm_ps (ps_mul nz₃ nz₁) (ps_mul nz₃ nz₂).
+Lemma eq_norm_ps_mul_compat_l : ∀ ps₁ ps₂ ps₃,
+  eq_norm_ps ps₁ ps₂
+  → eq_norm_ps (ps_mul ps₃ ps₁) (ps_mul ps₃ ps₂).
 Proof.
-intros nz₁ nz₂ nz₃ Heq.
+intros ps₁ ps₂ ps₃ Heq.
 induction Heq.
 constructor; simpl.
  rewrite H, H0; reflexivity.
@@ -781,12 +780,12 @@ destruct (Nbar.lt_dec (fin k) (stop a + fin n + stop b)) as [H₁| H₁].
  reflexivity.
 Qed.
 
-Lemma normalise_ps_mul_adjust_l : ∀ nz₁ nz₂ n k,
-  normalise_ps (ps_mul nz₁ nz₂) ≐
-  normalise_ps (ps_mul (adjust_ps n k nz₁) nz₂).
+Lemma normalise_ps_mul_adjust_l : ∀ ps₁ ps₂ n k,
+  normalise_ps (ps_mul ps₁ ps₂) ≐
+  normalise_ps (ps_mul (adjust_ps n k ps₁) ps₂).
 Proof.
-intros nz₁ nz₂ n k.
-remember (Pos.to_nat (ps_comden nz₂) * n)%nat as m eqn:Hm .
+intros ps₁ ps₂ n k.
+remember (Pos.to_nat (ps_comden ps₂) * n)%nat as m eqn:Hm .
 rewrite ps_norm_adjust_eq with (n := m) (k := k); subst m.
 unfold ps_mul; simpl.
 unfold adjust_ps; simpl.
@@ -809,17 +808,17 @@ rewrite series_mul_comm, Pos.mul_comm, series_mul_comm.
 reflexivity.
 Qed.
 
-Lemma nz_norm_mul_compat_r : ∀ nz₁ nz₂ nz₃,
-  normalise_ps nz₁ ≐ normalise_ps nz₂
-  → normalise_ps (ps_mul nz₁ nz₃) ≐ normalise_ps (ps_mul nz₂ nz₃).
+Lemma ps_norm_mul_compat_r : ∀ ps₁ ps₂ ps₃,
+  normalise_ps ps₁ ≐ normalise_ps ps₂
+  → normalise_ps (ps_mul ps₁ ps₃) ≐ normalise_ps (ps_mul ps₂ ps₃).
 Proof.
-intros nz₁ nz₂ nz₃ Heq.
+intros ps₁ ps₂ ps₃ Heq.
 remember Heq as Heqv; clear HeqHeqv.
-remember (normalise_ps nz₁) as ps₁ eqn:Hps₁  in Heq.
-remember (normalise_ps nz₂) as ps₂ eqn:Hps₂  in Heq.
+remember (normalise_ps ps₁) as nps₁ eqn:Hps₁  in Heq.
+remember (normalise_ps ps₂) as nps₂ eqn:Hps₂  in Heq.
 symmetry in Hps₁, Hps₂.
-remember (null_coeff_range_length rng (ps_terms nz₁) 0) as m₁ eqn:Hm₁ .
-remember (null_coeff_range_length rng (ps_terms nz₂) 0) as m₂ eqn:Hm₂ .
+remember (null_coeff_range_length rng (ps_terms ps₁) 0) as m₁ eqn:Hm₁ .
+remember (null_coeff_range_length rng (ps_terms ps₂) 0) as m₂ eqn:Hm₂ .
 symmetry in Hm₁, Hm₂.
 destruct m₁ as [m₁| ].
  apply normalised_exists_adjust in Hps₁.
@@ -827,12 +826,12 @@ destruct m₁ as [m₁| ].
    apply normalised_exists_adjust in Hps₂.
     destruct Hps₁ as (n₁, (k₁, Hps₁)).
     destruct Hps₂ as (n₂, (k₂, Hps₂)).
-    apply eq_norm_ps_mul_compat_r with (nz₃ := nz₃) in Hps₁.
-    apply eq_norm_ps_mul_compat_r with (nz₃ := nz₃) in Hps₂.
+    apply eq_norm_ps_mul_compat_r with (ps₃ := ps₃) in Hps₁.
+    apply eq_norm_ps_mul_compat_r with (ps₃ := ps₃) in Hps₂.
     rewrite Hps₁, Hps₂.
     rewrite <- normalise_ps_mul_adjust_l.
     rewrite <- normalise_ps_mul_adjust_l.
-    apply eq_norm_ps_mul_compat_r with (nz₃ := nz₃) in Heq.
+    apply eq_norm_ps_mul_compat_r with (ps₃ := ps₃) in Heq.
     rewrite Heq; reflexivity.
 
     intros H; rewrite Hm₂ in H; discriminate H.
@@ -850,8 +849,8 @@ destruct m₁ as [m₁| ].
  apply eq_norm_ps_adjust_zero_neg_zero in Hm₂.
  destruct Hm₁ as (n₁, (n₂, (k₁, (k₂, Hm₁)))).
  destruct Hm₂ as (n₃, (n₄, (k₃, (k₄, Hm₂)))).
- apply eq_norm_ps_mul_compat_r with (nz₃ := nz₃) in Hm₁.
- apply eq_norm_ps_mul_compat_r with (nz₃ := nz₃) in Hm₂.
+ apply eq_norm_ps_mul_compat_r with (ps₃ := ps₃) in Hm₁.
+ apply eq_norm_ps_mul_compat_r with (ps₃ := ps₃) in Hm₂.
  rewrite normalise_ps_mul_adjust_l with (n := n₁) (k := k₁).
  rewrite Hm₁; symmetry.
  rewrite normalise_ps_mul_adjust_l with (n := n₃) (k := k₃).
@@ -866,11 +865,8 @@ Theorem ps_mul_compat_r : ∀ ps₁ ps₂ ps₃,
   → (ps₁ * ps₃ = ps₂ * ps₃)%ps.
 Proof.
 intros ps₁ ps₂ ps₃ H₁₂.
-destruct ps₃ as (nz₃).
-destruct ps₁ as (nz₁).
-destruct ps₂ as (nz₂).
 constructor.
-apply nz_norm_mul_compat_r.
+apply ps_norm_mul_compat_r.
 inversion H₁₂; assumption.
 Qed.
 
@@ -888,7 +884,7 @@ Add Parametric Morphism : ps_mul
   with signature eq_norm_ps ==> eq_norm_ps ==> eq_norm_ps
   as ps_norm_mul_morph.
 Proof.
-intros nz₁ nz₃ Heq₁ nz₂ nz₄ Heq₂.
+intros ps₁ ps₃ Heq₁ ps₂ ps₄ Heq₂.
 rewrite eq_norm_ps_mul_compat_l; [ idtac | eassumption ].
 rewrite eq_norm_ps_mul_compat_r; [ idtac | eassumption ].
 reflexivity.
