a0fc3630d0cbd6ecda6c7c4cac7a38345d9e9ae3
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2013-12-31 01:29:56 +0100

    - restarting idea of polynomial of branch new-pol

--- coq/Polynomial.v
+++ coq/Polynomial.v
@@ -1,7 +1,5 @@
 (* Polynomial.v *)
 
-(* may be removed one day, using only Fpolynomial.v *)
-
 Require Import Utf8.
 Require Import QArith.
 
@@ -11,91 +9,51 @@ Notation "[ ]" := nil.
 Notation "[ x ; .. ; y … l ]" := (cons x .. (cons y l) ..).
 Notation "[ x ]" := (cons x nil).
 
-Record polynomial α := mkpol { al : list α; an : α }.
+Record polynomial α := mkpol { bl : list α }.
 
-Fixpoint pol_add_loop α (add_coeff : α → α → α) an₁ an₂ al₁ al₂ :=
+(* addition *)
+
+Fixpoint pol_add_loop α (add_coeff : α → α → α) al₁ al₂ :=
   match al₁ with
-  | [] =>
-      match al₂ with
-      | [] => {| al := []; an := add_coeff an₁ an₂ |}
-      | [a₂ … bl₂] => {| al := [add_coeff an₁ a₂ … bl₂]; an := an₂ |}
-      end
+  | [] => al₂
   | [a₁ … bl₁] =>
       match al₂ with
-      | [] => {| al := [add_coeff a₁ an₂ … bl₁]; an := an₁ |}
-      | [a₂ … bl₂] =>
-          let r := pol_add_loop add_coeff an₁ an₂ bl₁ bl₂ in
-          {| al := [add_coeff a₁ a₂ … al r]; an := an r |}
+      | [] => al₁
+      | [a₂ … bl₂] => [add_coeff a₁ a₂ … pol_add_loop add_coeff bl₁ bl₂]
       end
   end.
 
 Definition pol_add α (add_coeff : α → α → α) pol₁ pol₂ :=
-  pol_add_loop add_coeff (an pol₁) (an pol₂) (al pol₁) (al pol₂).
+  {| bl := pol_add_loop add_coeff (bl pol₁) (bl pol₂) |}.
 
-Fixpoint insert_pol_term α (add_coeff : α → α → α) c₁ p₁ ml :=
-  match ml with
-  | [] => [(c₁, p₁)]
-  | [(c₂, p₂) … ml₂] =>
-      match nat_compare p₁ p₂ with
-      | Eq => [(add_coeff c₁ c₂, p₂) … ml₂]
-      | Lt => [(c₁, p₁) … ml]
-      | Gt => [(c₂, p₂) … insert_pol_term add_coeff c₁ p₁ ml₂]
-      end
+(* multiplication *)
+
+Fixpoint sigma_aux α zero_v (add_v : α → α → _) b len g :=
+  match len with
+  | O => zero_v
+  | S len₁ => add_v (g b) (sigma_aux zero_v add_v (S b) len₁ g)
   end.
 
-Fixpoint combine_pol α add_coeff (mul_coeff : α → α → α) c₁ pow₁ pow₂ ml
-    cn cl :=
-  let p := (pow₁ + pow₂)%nat in
-  match cl with
-  | [] =>
-      let c := mul_coeff c₁ cn in
-      insert_pol_term add_coeff c p ml
-  | [c₂ … cl₂] =>
-      let c := mul_coeff c₁ c₂ in
-      let ml := insert_pol_term add_coeff c p ml in
-      combine_pol add_coeff mul_coeff c₁ pow₁ (S pow₂) ml cn cl₂
+Definition sigma α zero_v (add_v : α → α → _) b e g :=
+  sigma_aux zero_v add_v b (S e - b) g.
+
+Fixpoint pol_convol_mul α zero_v add_v (mul_v : α → α → _) al₁ al₂ i len :=
+  match len with
+  | O => []
+  | S len₁ =>
+      [sigma zero_v add_v O i
+         (λ j, mul_v (List.nth j al₁ zero_v) (List.nth (i - j) al₂ zero_v)) …
+       pol_convol_mul zero_v add_v mul_v al₁ al₂ (S i) len₁]
   end.
 
-Fixpoint mul_loop α (add_coeff : α → α → α) mul_coeff ml pow₁ cn₂ cl₂
-    cn₁ cl₁ :=
-  match cl₁ with
-  | [] => combine_pol add_coeff mul_coeff cn₁ pow₁ 0 ml cn₂ cl₂
-  | [c … cl] =>
-      let ml := combine_pol add_coeff mul_coeff c pow₁ 0 ml cn₂ cl₂ in
-      mul_loop add_coeff mul_coeff ml (S pow₁) cn₂ cl₂ cn₁ cl
-  end.
-
-Fixpoint make_pol α (zero_coeff : α) pow ml n :=
-  match n with
-  | O => ([], zero_coeff)
-  | S n₁ =>
-      match ml with
-      | [] => ([], zero_coeff)
-      | [(c, p)] =>
-          if eq_nat_dec p pow then ([], c)
-          else
-            let (cl, cn) := make_pol zero_coeff (S pow) [(c, p)] n₁ in
-            ([zero_coeff … cl], cn)
-      | [(c, p) … ml₁] =>
-          if eq_nat_dec p pow then
-            let (cl, cn) := make_pol zero_coeff (S pow) ml₁ n₁ in
-            ([c … cl], cn)
-          else
-            let (cl, cn) := make_pol zero_coeff (S pow) ml n₁ in
-            ([zero_coeff … cl], cn)
-      end
-  end.
-
-Definition pol_mul α (zero_coeff : α) add_coeff mul_coeff pol₁ pol₂ :=
-  let ml :=
-    mul_loop add_coeff mul_coeff [] 0 (an pol₂) (al pol₂) (an pol₁) (al pol₁)
-  in
-  let (cl, cn) := make_pol zero_coeff 0 ml (List.length ml) in
-  {| al := cl; an := cn |}.
+Definition pol_mul α (zero_v : α) add_v mul_v pol₁ pol₂ :=
+  {| bl :=
+       pol_convol_mul zero_v add_v mul_v (bl pol₁) (bl pol₂) O
+         (max (List.length (bl pol₁)) (List.length (bl pol₂))) |}.
 
 (* Horner's algorithm *)
 Definition apply_poly α β γ
     (zero_plus_v : β → α) (add_v_coeff : α → β → α) (mul_v_x : α → γ → α)
     (pol : polynomial β) (x : γ) :=
   List.fold_right (λ c accu, add_v_coeff (mul_v_x accu x) c)
-    (zero_plus_v (an pol)) (al pol).
+    (zero_plus_v (bl pol)) (bl pol).
