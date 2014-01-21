f75a1b4e50279b7dfc095fcf6a6591c6e918de68
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-01-22 00:41:39 +0100

    -

--- coq/AlgCloCharPol.v
+++ coq/AlgCloCharPol.v
@@ -9,22 +9,6 @@ Require Import CharactPolyn.
 
 Set Implicit Arguments.
 
-Fixpoint degree_plus_1_of_list α (is_zero : α → bool) (l : list α) :=
-  match l with
-  | [] => O
-  | [x … l₁] =>
-      match degree_plus_1_of_list is_zero l₁ with
-      | O => if is_zero x then O else 1%nat
-      | S n => S (S n)
-      end
-  end.
-
-Definition degree α is_zero (pol : polynomial α) :=
-  pred (degree_plus_1_of_list is_zero (al pol)).
-
-Definition apply_polynomial α (f : field α) :=
-  apply_poly (fld_zero f) (fld_add f) (fld_mul f).
-
 (* euclidean division of a polynomial by (x - c) *)
 
 Fixpoint list_mod_div_mono A zero (add : A → A → A) mul (c : A) al :=
@@ -87,12 +71,28 @@ Definition ith_der_on_fact_i A (mul : nat → A → A) pol i :=
 
 (* *)
 
+Fixpoint degree_plus_1_of_list α (is_zero : α → bool) (l : list α) :=
+  match l with
+  | [] => O
+  | [x … l₁] =>
+      match degree_plus_1_of_list is_zero l₁ with
+      | O => if is_zero x then O else 1%nat
+      | S n => S (S n)
+      end
+  end.
+
+Definition degree α is_zero (pol : polynomial α) :=
+  pred (degree_plus_1_of_list is_zero (al pol)).
+
+Definition apply_polynomial α (f : field α) :=
+  apply_poly (fld_zero f) (fld_add f) (fld_mul f).
+
 Record algeb_closed_field α :=
   { ac_field : field α;
-    ac_root : polynomial α → (α * nat);
     ac_is_zero : α → bool;
+    ac_root : polynomial α → α;
     ac_prop : ∀ pol, degree ac_is_zero pol ≥ 1
-      → (apply_polynomial ac_field pol (fst (ac_root pol)) .= ac_field
+      → (apply_polynomial ac_field pol (ac_root pol) .= ac_field
          .0 ac_field)%K }.
 
 Fixpoint poly_power α (f : field α) pol n :=
