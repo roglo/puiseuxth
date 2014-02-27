eb2ad76f9024f4cbd5f769f679f8c962ce5109d2
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-02-27 21:13:43 +0100

    -

--- coq/Slope_base.v
+++ coq/Slope_base.v
@@ -4,7 +4,7 @@ Require Import Utf8.
 Require Import QArith.
 
 Definition slope_expr pt₁ pt₂ :=
-  Qdiv (Qminus (snd pt₂) (snd pt₁)) (Qminus (fst pt₂) (fst pt₁)).
+  (snd pt₂ - snd pt₁) / (fst pt₂ - fst pt₁).
 
 Lemma fold_slope_expr : ∀ x₁ y₁ x₂ y₂,
   (y₂ - y₁) / (x₂ - x₁) = slope_expr (x₁, y₁) (x₂, y₂).
