1b60bc0a83a5224d41294948723e8caafd23f0bb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-04-02 09:13:10 +0200

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -213,7 +213,11 @@ eapply valuation_in_newton_segment with (h := h) (αh := αh) in Hval; eauto .
   rewrite Z.min_l.
    rewrite Z.mul_add_distr_r.
    apply Z.add_lt_mono_l.
-   (* bizarre... *)
+   apply null_coeff_range_length_iff in Hm.
+   apply null_coeff_range_length_iff in Hn.
+   unfold null_coeff_range_length_prop in Hm, Hn.
+   simpl in Hm.
+   remember ps_add as f; simpl in Hn; subst f.
 bbb.
 
 (* old stuff; to be used later perhaps *)
