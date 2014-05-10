ddc2364d56981f13dd7d3f37fe4b522fe97f0142
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-05-10 08:13:47 +0200

    root stub

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -1,12 +1,18 @@
 (* Puiseux.v *)
 
 Require Import Utf8.
-(*
-Require Import QArith.
-Require Import NPeano.
-Require Import Sorted.
-*)
 
+Require Import Misc.
+Require Import Field.
+Require Import Fpolynomial.
+Require Import Puiseux_series.
+Require Import Puiseux_base.
 Require Import F1Prop.
 
 Set Implicit Arguments.
+
+Definition root α {R : ring α} (pol : polynomial (puiseux_series α)) :=
+  match newton_segments pol with
+  | [] => 0%ps
+  | [ns … _] => 0%ps
+  end.
