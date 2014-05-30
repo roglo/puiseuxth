21f7be5143c3b3f2e69ff931d549fa30784cfb65
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-05-30 05:51:18 +0200

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -385,6 +385,23 @@ symmetry in Hr.
 unfold root_multiplicity; simpl.
 rewrite Nat.sub_diag; simpl.
 rewrite skipn_pad; simpl.
+remember (al pol₁) as la eqn:Hla .
+unfold term_of_point; rewrite <- Hla.
+unfold next_pol in Hpol₁.
+remember Hla as H; clear HeqH.
+rewrite Hpol₁ in H; simpl in H.
+clear Hpol₁; rename H into Hpol₁.
+unfold ps_poly_nth in Hps₀; rewrite <- Hla in Hps₀.
+unfold newton_segments, points_of_ps_polynom in Hns₁; rewrite <- Hla in Hns₁.
+unfold Φq, summation_ah_xh_pol, characteristic_polynomial in Hc₂.
+unfold term_of_point in Hc₂; rewrite <- Hla in Hc₂.
+unfold ps_poly_nth in Hnneg; rewrite <- Hla in Hnneg.
+unfold ps_poly_nth in Hpos; rewrite <- Hla in Hpos.
+unfold ps_poly_nth in Hz; rewrite <- Hla in Hz.
+clear pol₁ Hla; simpl in Hc₂.
+unfold poly_left_shift in Hc₂; simpl in Hc₂.
+rewrite skipn_pad, Nat.sub_diag, list_pad_0 in Hc₂.
+assert (nat_num (fst (ini_pt ns₁)) = 0)%nat as Hini.
 bbb.
 
 assert (ns₁ ∈ newton_segments pol₁) as Hns₁in.
