fef240c36a1e972c1513b7c2c9c3f90336a5da32
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-06-08 17:35:59 +0200

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -927,6 +927,26 @@ Fixpoint ps_poly_root m pol ns :=
       end
   end.
 
+Lemma yyy : ∀ pol ns m ps,
+  ns ∈ newton_segments pol
+  → ps_poly_root m pol ns = Some ps
+    → (ps_pol_apply pol ps = 0)%ps.
+Proof.
+intros pol ns m ps Hns Hps.
+revert pol ns ps Hns Hps.
+induction m; intros; [ discriminate Hps | simpl in Hps ].
+remember (ac_root (Φq pol ns)) as c eqn:Hc .
+remember (next_pol pol (β ns) (γ ns) c) as pol₁ eqn:Hpol₁ .
+remember (List.hd phony_ns (newton_segments pol₁)) as ns₁ eqn:Hns₁ .
+remember (ps_poly_root m pol₁ ns₁) as pso eqn:Hpso .
+symmetry in Hpso.
+destruct pso as [ps₁| ]; [ idtac | discriminate Hps ].
+injection Hps; clear Hps; intros Hps.
+apply IHm in Hpso.
+ symmetry in Hps.
+ eapply f₁_root_f_root; eassumption.
+bbb.
+
 Lemma zzz : ∀ pol ns m,
   ns = List.hd phony_ns (newton_segments pol)
   → polydromy_if_r_reaches_one acf m pol ≠ 0%nat
