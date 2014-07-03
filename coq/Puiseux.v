917d4700f8abd3709e50f2389080dead3923ac02
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-07-03 17:26:00 +0200

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -405,7 +405,7 @@ intros c q Hc.
 constructor; simpl.
  unfold normalise_ps; simpl.
  rewrite fold_series_const.
-rewrite null_coeff_range_length_const; auto; simpl.
+ rewrite null_coeff_range_length_const; auto; simpl.
  rewrite Z.add_0_r.
  rewrite greatest_series_x_power_series_const.
  unfold gcd_ps; simpl.
@@ -416,8 +416,46 @@ rewrite null_coeff_range_length_const; auto; simpl.
  pose proof (Z.ggcd_correct_divisors q₁ (' q₂)) as H.
  remember (Z.ggcd q₁ (' q₂)) as g.
  destruct g as (g, (aa, bb)).
- destruct H as (Hq₁, Hq₂).
- simpl.
+ destruct H as (Hq₁, Hq₂); simpl.
+ rewrite Hq₁, Hq₂; simpl.
+ pose proof (Z.ggcd_gcd q₁ (' q₂)) as Hg.
+ rewrite <- Heqg in Hg; simpl in Hg.
+ assert (0 <= g)%Z as Hgnn by (subst g; apply Z.gcd_nonneg).
+ assert (g ≠ 0)%Z as Hgnz.
+  subst g; intros H.
+  apply Z.gcd_eq_0_r in H.
+  revert H; apply Pos2Z_ne_0.
+
+  rewrite Z.gcd_mul_mono_l_nonneg; auto.
+  assert (Z.gcd aa bb = 1)%Z as Hg1.
+   apply Z.mul_reg_l with (p := g); auto.
+   rewrite Z.mul_1_r.
+   rewrite <- Z.gcd_mul_mono_l_nonneg; auto.
+   rewrite <- Hq₁, <- Hq₂.
+   symmetry; assumption.
+
+   rewrite Z.abs_eq.
+    rewrite Z.div_mul_cancel_l; auto.
+     rewrite Hg1, Z.div_1_r; reflexivity.
+
+     rewrite Hg1; intros H; discriminate H.
+
+    apply Z.mul_nonneg_nonneg; auto.
+    rewrite Hg1; apply Z.le_0_1.
+
+ unfold normalise_ps; simpl.
+ rewrite fold_series_const.
+ rewrite null_coeff_range_length_const; auto; simpl.
+ rewrite greatest_series_x_power_series_const.
+ unfold gcd_ps; simpl.
+ rewrite Z.add_0_r; simpl.
+ rewrite Z.gcd_0_r; simpl.
+ unfold Qred; simpl.
+ destruct q as (q₁, q₂).
+ pose proof (Z.ggcd_correct_divisors q₁ (' q₂)) as H.
+ remember (Z.ggcd q₁ (' q₂)) as g.
+ destruct g as (g, (aa, bb)).
+ destruct H as (Hq₁, Hq₂); simpl.
  rewrite Hq₁, Hq₂; simpl.
  pose proof (Z.ggcd_gcd q₁ (' q₂)) as Hg.
  rewrite <- Heqg in Hg; simpl in Hg.
