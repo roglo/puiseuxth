23abb1ec2fdfc1bf749bfae711edcc806ec6393a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-07-23 11:07:36 +0200

    -

--- coq/Puiseux.v
+++ coq/Puiseux.v
@@ -2207,6 +2207,94 @@ induction n; intros.
           rewrite <- find_coeff_add with (dp := g₂).
           rewrite Nat.add_0_l, Nat.sub_add; auto.
           symmetry.
+(**)
+          rewrite <- Heqid.
+          simpl.
+          destruct (ps_zerop R (ps_poly_nth 0 pol₂)); try contradiction.
+          clear n.
+          remember (Nat.compare g₂ (S i)) as cmp eqn:Hcmp .
+          symmetry in Hcmp.
+          destruct cmp; auto.
+          remember (ac_root (Φq pol₂ ns₂)) as c₂ eqn:Hc₂ .
+          remember (next_pol pol₂ (β ns₂) (γ ns₂) c₂) as pol₃ eqn:Hpol₃ .
+          remember (List.hd phony_ns (newton_segments pol₃)) as ns₃ eqn:Hns₃ .
+          remember (next_pow g₂ ns₃ m₁) as g₂₃ eqn:Hg₂₃ .
+          apply nat_compare_lt in Hcmp.
+          destruct i.
+           apply Nat.lt_1_r in Hcmp.
+           rewrite Hnpow in Hcmp.
+           exfalso; revert Hcmp; apply Pos2Nat_ne_0.
+
+           destruct id.
+            exfalso; fast_omega Hcmp Heqid.
+
+            simpl.
+            clear H₁.
+            destruct (ps_zerop R (ps_poly_nth 0 pol₃)) as [H₁| H₁]; auto.
+            remember (Nat.compare g₂₃ (S (S i))) as cmp₁ eqn:Hcmp₁ .
+            symmetry in Hcmp₁.
+            destruct cmp₁; auto.
+            remember (ac_root (Φq pol₃ ns₃)) as c₃ eqn:Hc₃ .
+            remember (next_pol pol₃ (β ns₃) (γ ns₃) c₃) as pol₄ eqn:Hpol₄ .
+            remember (List.hd phony_ns (newton_segments pol₄)) as ns₄
+             eqn:Hns₄ .
+            remember (next_pow g₂₃ ns₄ m₁) as g₂₃₄.
+            apply nat_compare_lt in Hcmp₁.
+            destruct i.
+             destruct g₂.
+              symmetry in Hnpow.
+              exfalso; revert Hnpow; apply Pos2Nat_ne_0.
+
+              replace id with O by omega.
+              reflexivity.
+
+             destruct id.
+              simpl.
+              destruct (ps_zerop R (ps_poly_nth 0 pol₄)) as [H₄| H₄]; auto.
+              remember (Nat.compare g₂₃₄ (S (S (S i)))) as cmp₂ eqn:Hcmp₂ .
+              symmetry in Hcmp₂.
+              destruct cmp₂; auto.
+               apply nat_compare_eq in Hcmp₂.
+               unfold next_pow in Hg₂₃; simpl in Hg₂₃.
+               remember Hns₃ as H; clear HeqH.
+               apply exists_ini_pt_nat_fst_seg in H.
+               destruct H as (j₃, (αj₃, Hini₃)).
+               remember Hns₃ as H; clear HeqH.
+               apply exists_fin_pt_nat_fst_seg in H.
+               destruct H as (k₃, (αk₃, Hfin₃)).
+               remember Hns₃ as Hns₃₁; clear HeqHns₃₁.
+               remember Hns₁₁ as Hr₂; clear HeqHr₂.
+               eapply multiplicity_1_remains in Hr₂; eauto .
+               remember Hns₂₁ as H; clear HeqH.
+               eapply r_1_j_0_k_1 in H; eauto .
+               destruct H as (Hj₃, (Hk₃, (Hαj₃, (Hαk₃, Hoth₃)))).
+               subst j₃ k₃.
+               unfold Qlt in Hαj₃; simpl in Hαj₃.
+               unfold Qeq in Hαk₃; simpl in Hαk₃.
+               rewrite Z.mul_1_r in Hαj₃, Hαk₃.
+               rewrite Hini₃, Hfin₃ in Hg₂₃; simpl in Hg₂₃.
+               rewrite Hαk₃ in Hg₂₃; simpl in Hg₂₃.
+               rewrite Z.add_0_r, Z.mul_1_r in Hg₂₃.
+               do 2 rewrite Pos.mul_1_r in Hg₂₃.
+               rewrite Z.mul_shuffle0 in Hg₂₃.
+               rewrite Pos2Z.inj_mul in Hg₂₃.
+               rewrite Z.div_mul_cancel_r in Hg₂₃; auto.
+               eapply hd_newton_segments in Hns₃₁; eauto .
+               remember Hns₃₁ as H; clear HeqH.
+               eapply num_m_den_is_pos with (m := m₁) in H; eauto .
+                exfalso; omega.
+
+                replace m₁ with (m₁ * 1)%positive by apply Pos.mul_1_r.
+                eapply next_pol_in_K_1_mq with (pol := pol₂); eauto .
+                symmetry.
+                replace m₁ with (m₁ * 1)%positive by apply Pos.mul_1_r.
+                eapply q_eq_1 with (pol := pol₁) (pol₁ := pol₂); eauto .
+                 rewrite Heqm₁; assumption.
+
+                 rewrite Pos.mul_1_r; assumption.
+
+               apply nat_compare_lt in Hcmp₂.
+bbb.
           rewrite qqq with (d := (g₂ - 1)%nat).
            rewrite Nat_sub_sub_distr.
             rewrite Nat.add_1_r.
