c4b4bf8cf7c42d9ea4fe97af8bfa39fd3690219c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-08-20 12:15:49 +0200

    -

---
+++ coq/Work.v
@@ -0,0 +1,44 @@
+(* Work.v *)
+
+Require Import Utf8 QArith NPeano Sorting.
+
+Require Import Misc.
+Require Import SlopeMisc.
+Require Import Slope_base.
+Require Import Qbar.
+Require Import Nbar.
+Require Import Field.
+Require Import Fpolynomial.
+Require Import Fsummation.
+Require Import Newton.
+Require Import ConvexHullMisc.
+Require Import ConvexHull.
+Require Import PolyConvexHull.
+Require Import NotInSegment.
+Require Import Power_series.
+Require Import Puiseux_series.
+Require Import Ps_add.
+Require Import Ps_mul.
+Require Import Ps_div.
+Require Import PSpolynomial.
+Require Import Puiseux_base.
+Require Import AlgCloCharPol.
+Require Import CharactPolyn.
+Require Import F1Eq.
+Require Import PosOrder.
+Require Import F1Prop.
+Require Import InK1m.
+Require Import Q_field.
+Require Import RootHeadTail.
+Require Import RootWhenR1.
+
+Set Implicit Arguments.
+
+Section theorems.
+
+Variable α : Type.
+Variable R : ring α.
+Variable K : field R.
+Variable acf : algeb_closed_field K.
+
+bbb.
