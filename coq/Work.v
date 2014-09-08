cc792ebe91971319713baaa932855a794165b71e
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-09-09 01:42:16 +0200

    -

--- coq/Work.v
+++ coq/Work.v
@@ -450,7 +450,7 @@ destruct H as (Hj₁, (Hr₁, (Hr, _))).
 transitivity k₁; auto.
 Qed.
 
-Theorem yyy : ∀ pol ns r n,
+Theorem r_le_eq_incl : ∀ pol ns r n,
   ns ∈ newton_segments pol
   → nth_r 0 pol ns = r
   → (∀ i, i ≤ n → (ps_poly_nth 0 (nth_pol i pol ns) ≠ 0)%ps)
@@ -463,58 +463,56 @@ apply Hri in H.
 apply Nat.le_antisymm; auto.
 clear H.
 revert pol ns r n Hns Hr₀ Hnz Hri Hin.
-induction i; intros.
- rewrite <- Hr₀; reflexivity.
+induction i; intros; [ rewrite <- Hr₀; reflexivity | idtac ].
+destruct n; [ exfalso; revert Hin; apply Nat.nle_succ_0 | idtac ].
+remember Hin as H; clear HeqH.
+apply Hri in H.
+simpl in H; simpl.
+remember (ac_root (Φq pol ns)) as c eqn:Hc .
+remember (next_pol pol (β ns) (γ ns) c) as pol₁ eqn:Hpol₁ .
+remember (List.hd phony_ns (newton_segments pol₁)) as ns₁ eqn:Hns₁ .
+eapply IHi; eauto .
+ eapply List_hd_in; eauto .
+ clear H.
+ remember Hns as H; clear HeqH.
+ eapply next_has_root_0_or_newton_segments in H; eauto .
+ destruct H as [H₁| H₁].
+  assert (1 ≤ S n)%nat as H by apply le_n_S, Nat.le_0_l.
+  apply Hnz in H; contradiction.
 
- destruct n.
-  exfalso; revert Hin; apply Nat.nle_succ_0.
+  simpl in H₁.
+  rewrite <- Hc, <- Hpol₁ in H₁; auto.
 
-  remember Hin as H; clear HeqH.
-  apply Hri in H.
-  simpl in H; simpl.
-  remember (ac_root (Φq pol ns)) as c eqn:Hc .
-  remember (next_pol pol (β ns) (γ ns) c) as pol₁ eqn:Hpol₁ .
-  remember (List.hd phony_ns (newton_segments pol₁)) as ns₁ eqn:Hns₁ .
-  eapply IHi; eauto .
-bbb.
+ apply Nat.le_antisymm.
+  clear H.
+  remember Hns as H; clear HeqH.
+  eapply r₁_le_r₀ in H; eauto .
+   rewrite Hr₀ in H; simpl in H.
+   rewrite <- Hc, <- Hpol₁, <- Hns₁ in H; auto.
 
-   Focus 2.
-   apply Nat.le_antisymm.
-    clear H.
-    remember Hns as H; clear HeqH.
-    eapply r₁_le_r₀ in H; eauto .
-     rewrite Hr₀ in H; simpl in H.
-     rewrite <- Hc, <- Hpol₁, <- Hns₁ in H; auto.
-
-     clear H.
-     assert (1 ≤ S n)%nat as H by apply le_n_S, Nat.le_0_l.
-     apply Hnz in H; auto.
-
-    clear H.
-    assert (1 ≤ S n)%nat as H by apply le_n_S, Nat.le_0_l.
-    apply Hri in H; simpl in H.
-    rewrite <- Hc, <- Hpol₁, <- Hns₁ in H.
-    auto.
-
-   Focus 2.
    clear H.
-   intros j Hji.
-   apply Nat.succ_le_mono in Hji.
-   eapply Nat.le_trans in Hin; eauto .
-   apply Hnz in Hin; simpl in Hin.
-   rewrite <- Hc, <- Hpol₁, <- Hns₁ in Hin.
-   auto.
+   assert (1 ≤ S n)%nat as H by apply le_n_S, Nat.le_0_l.
+   apply Hnz in H; auto.
 
-   Focus 2.
-   clear H.
-   intros j Hji.
-   apply Nat.succ_le_mono in Hji.
-   eapply Nat.le_trans in Hin; eauto .
-   apply Hri in Hin; simpl in Hin.
-   rewrite <- Hc, <- Hpol₁, <- Hns₁ in Hin.
-   auto.
-bbb.
-*)
+  clear H.
+  assert (1 ≤ S n)%nat as H by apply le_n_S, Nat.le_0_l.
+  apply Hri in H; simpl in H.
+  rewrite <- Hc, <- Hpol₁, <- Hns₁ in H; auto.
+
+ clear H.
+ intros j Hji.
+ apply Nat.succ_le_mono in Hji.
+ eapply Nat.le_trans in Hin; eauto .
+ apply Hnz in Hin; simpl in Hin.
+ rewrite <- Hc, <- Hpol₁, <- Hns₁ in Hin; auto.
+
+ clear H.
+ intros j Hji.
+ apply Nat.succ_le_mono in Hji.
+ eapply Nat.le_trans in Hin; eauto .
+ apply Hri in Hin; simpl in Hin.
+ rewrite <- Hc, <- Hpol₁, <- Hns₁ in Hin; auto.
+Qed.
 
 Theorem zzz : ∀ pol ns c pol₁,
   ns ∈ newton_segments pol
