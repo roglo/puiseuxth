3cfe67b69ee69f90727952bc508ac790e8f808eb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-09-22 11:14:26 +0200

    -

--- coq/Bug.v
+++ coq/Bug.v
@@ -22,16 +22,12 @@ Class field α (rng_ring : ring α) := { fld_inv : α → α }.
 Import Coq.QArith.QArith.
 
 Notation "[ ]" := nil.
-Notation "[ x ; .. ; y … l ]" := (cons x .. (cons y l) ..).
 Notation "[ x ]" := (cons x nil).
 
 Definition Qnat i := Z.of_nat i # 1.
 
 Inductive lap_eq {α} {r : ring α} : list α → list α → Prop :=
-  | lap_eq_cons : ∀ x₁ x₂ l₁ l₂,
-      (x₁ = x₂)%K
-      → lap_eq l₁ l₂
-        → lap_eq [x₁ … l₁] [x₂ … l₂].
+  | lap_eq_cons : ∀ x₁ x₂ l₁ l₂, lap_eq (x₁ :: l₁) (x₂ :: l₂).
 
 Theorem lap_eq_refl {α} {r : ring α} : reflexive _ lap_eq.
 Admitted.
@@ -63,12 +59,6 @@ Theorem lap_eq_0 : lap_eq [0%K] [].
 Admitted.
 
 End lap.
-
-Inductive Nbar : Set :=
-  | fin : ∀ x : nat, Nbar
-  | inf : Nbar.
-
-Notation "∞" := inf.
 
 Record power_series α := series { terms : nat → α }.
 
@@ -102,15 +92,6 @@ Inductive eq_ps {α} {r : ring α} {K : field r} :
 
 Notation "a = b" := (eq_ps a b) : ps_scope.
 Notation "0" := ps_zero : ps_scope.
-
-Section other_theorems.
-
-Variable α : Type.
-
-Definition cm (ps₁ ps₂ : puiseux_series α) :=
-  (ps_polydo ps₁ * ps_polydo ps₂)%positive.
-
-End other_theorems.
 
 Definition ps_ring α (R : ring α) (K : field R) : ring (puiseux_series α).
 exact ({| rng_zero := ps_zero;
