da4925a3f1270de12c1bde5fe325a1ccf4a34de5
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-06-06 10:44:01 +0200

    -

--- coq/Q_field.v
+++ coq/Q_field.v
@@ -4,9 +4,12 @@ From Stdlib Require Import Utf8.
 
 Require Import A_QArith.
 Require Import Field2.
+Open Scope Q_scope.
 
+Check Q.add_opp_l.
+...
 Theorem Qplus_opp_l : ∀ a, -a + a == 0.
-Proof. intros a; rewrite Qplus_comm; apply Qplus_opp_r. Qed.
+Proof. intros a; rewrite Q.add_comm; apply Q.add_opp_r. Qed.
 
 Theorem Qplus_compat_l : ∀ a b c, a == b → c + a == c + b.
 Proof. intros a b c H; apply Qplus_inj_l; assumption. Qed.
