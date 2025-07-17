503314977d3d388fdd5ae0a941545abec8a7fe01
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-07-17 03:25:48 +0200

    -

--- coq/farey.v
+++ coq/farey.v
@@ -456,38 +456,161 @@ Theorem g_aux_eq_f_aux_eq : ∀ n a b itf itg,
   → f_aux itf n = (a / Nat.gcd a b, b / Nat.gcd a b).
 Proof.
 intros * Hbz Hitf Hitg Hab.
-revert n a b Hbz itg Hitf Hitg Hab.
-induction itf; intros; [ easy | ].
-cbn - [ Nat.div ].
-destruct (Nat.eq_dec n 0) as [Hnz| Hnz]. {
-  move Hnz at top; subst n; clear Hitf.
+revert n a b Hbz itf Hitf Hitg Hab.
+induction itg; intros. {
+  cbn in Hab; subst n.
+  apply Nat.max_lub_r in Hitg.
+  now apply Nat.le_0_r in Hitg.
+}
+cbn - [ Nat.mul ] in Hab.
+destruct (Nat.eq_dec b 0) as [H| H]; [ easy | clear H ].
+destruct (lt_dec a b) as [Hlab| Hlab]. {
+  rewrite Nat.max_r in Hitg; [ | now apply Nat.lt_le_incl ].
+  destruct itf; [ easy | ].
+  cbn - [ Nat.div ].
+  destruct (Nat.eq_dec n 0) as [Hnz| Hnz]. {
+    move Hnz at top; subst n.
+    clear Hitf.
+    apply Nat.mul_eq_0_r in Hab; [ | easy ].
+    apply eq_g_aux_0 in Hab.
+    destruct Hab as [Hab| Hab]; [ lia | ].
+    destruct Hab as [Hab| Hab]. {
+      subst a; cbn.
+      rewrite Nat.Div0.div_0_l.
+      f_equal; symmetry.
+      now apply Nat.div_same.
+    }
+    destruct (le_dec (b - a) a) as [Hbba| Hbba]; [ lia | ].
+    apply Nat.nle_gt in Hbba.
+    rewrite Nat.max_l in Hab; [ | now apply Nat.lt_le_incl ].
+    destruct a; [ | lia ].
+    cbn.
+    rewrite Nat.Div0.div_0_l.
+    f_equal; symmetry.
+    now apply Nat.div_same.
+  }
+  remember (f_aux itf (n / 2)) as ab eqn:Hab'.
+  symmetry in Hab'.
+  destruct ab as (a', b').
+  remember (Nat.even n) as en eqn:Hen.
+  symmetry in Hen.
+  destruct en. {
+    apply Nat.even_EvenT in Hen.
+    destruct Hen as (n', H).
+    move H at top; subst n; rename n' into n.
+    apply Nat.mul_reg_l in Hab; [ | easy ].
+    apply Nat.neq_mul_0 in Hnz.
+    destruct Hnz as (_, Hnz).
+    rewrite Nat.mul_comm, Nat.div_mul in Hab'; [ | easy ].
+    destruct (Nat.eq_dec a 0) as [Haz| Haz]. {
+      subst a; cbn.
+      now rewrite g_aux_0_r in Hab; symmetry in Hab.
+    }
+    apply IHitg with (itf := itf) in Hab; [ | easy | lia | lia ].
+    rewrite Nat.gcd_comm in Hab.
+    rewrite Nat.gcd_sub_diag_r in Hab; [ | now apply Nat.lt_le_incl ].
+    rewrite Hab in Hab'.
+    injection Hab'; clear Hab'; intros; subst a' b'.
+    progress f_equal.
+    specialize (Nat.gcd_divide_l a b) as H1.
+    specialize (Nat.gcd_divide_r a b) as H2.
+    destruct H1 as (a', Ha).
+    destruct H2 as (b', Hb).
+    rewrite Ha at 1 3.
+    rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_l in H ].
+    rewrite Hb at 1 4.
+    rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_l in H ].
+    rewrite <- Nat.mul_sub_distr_r.
+    rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_l in H ].
+    apply Nat.sub_add.
+    apply (Nat.mul_le_mono_pos_r _ _ (Nat.gcd a b)); [ | lia ].
+    apply Nat.neq_0_lt_0.
+    now intros H; apply Nat.gcd_eq_0_l in H.
+  }
+  destruct n; [ easy | clear Hnz ].
+  rewrite Nat.even_succ in Hen.
+  apply (f_equal negb) in Hen; cbn in Hen.
+  rewrite Nat.negb_odd in Hen.
+  apply Nat.even_EvenT in Hen.
+  destruct Hen as (n', H); subst n.
+  lia.
+}
+apply Nat.nlt_ge in Hlab.
+rewrite Nat.max_l in Hitg; [ | easy ].
+destruct n; [ lia | ].
+rewrite Nat.add_1_r in Hab.
+apply Nat.succ_inj in Hab.
+destruct itf; [ easy | ].
+apply Nat.succ_lt_mono in Hitf.
+destruct n. {
+  apply Nat.eq_mul_0_r in Hab; [ | easy ].
   apply eq_g_aux_0 in Hab.
   destruct Hab as [Hab| Hab]. {
-    subst a; cbn.
+    replace b with a in * by lia.
+    clear Hlab Hab.
+    rewrite Nat.gcd_diag.
     rewrite Nat.div_same; [ | easy ].
-    now destruct b.
+    now destruct itf.
   }
   destruct Hab as [Hab| Hab]; [ easy | ].
-  now apply Nat.nle_gt in Hab.
+  destruct (le_dec (a - b) b) as [Habb| Habb]; [ | lia ].
+  rewrite Nat.max_r in Hab; [ | easy ].
+  replace b with a in * by lia.
+  clear Hlab Habb.
+  rewrite Nat.gcd_diag.
+  rewrite Nat.div_same; [ | easy ].
+  now destruct itf.
 }
-remember (f_aux itf (n / 2)) as ab eqn:Hab'.
+cbn - [ Nat.div ].
+remember (f_aux itf (S (S n) / 2)) as ab eqn:Hab'.
 symmetry in Hab'.
 destruct ab as (a', b').
 remember (Nat.even n) as en eqn:Hen.
 symmetry in Hen.
 destruct en. {
-  destruct itg; [ now cbn in Hab; symmetry in Hab | ].
-  cbn - [ Nat.mul ] in Hab.
-  destruct (Nat.eq_dec b 0) as [H| H]; [ easy | clear H ].
-  destruct (lt_dec a b) as [Hlab| Hlab]. {
-    rewrite Nat.max_r in Hitg; [ | now apply Nat.lt_le_incl ].
-    apply Nat.even_EvenT in Hen.
-    destruct Hen as (n', H); move H at top; subst n.
-    apply Nat.mul_reg_l in Hab; [ | easy ].
-    rewrite Nat.mul_comm, Nat.div_mul in Hab'; [ | easy ].
-...
-    apply IHitf in Hab.
-...
+  apply Nat.even_EvenT in Hen.
+  destruct Hen as (n', H); lia.
+}
+destruct n; [ easy | ].
+rewrite Nat.even_succ in Hen.
+apply (f_equal negb) in Hen; cbn in Hen.
+rewrite Nat.negb_odd in Hen.
+apply Nat.even_EvenT in Hen.
+destruct Hen as (n', H); subst n; rename n' into n.
+replace (S (S (2 * n))) with (2 * (S n)) in Hab by lia.
+apply Nat.mul_reg_l in Hab; [ | easy ].
+replace (S (S (S (2 * n)))) with (1 + S n * 2) in Hab' by lia.
+rewrite Nat.div_add in Hab'; [ | easy ].
+cbn in Hab'.
+destruct (Nat.eq_dec b (S itg)) as [Hbitg| Hbitg]. {
+  rewrite <- Hbitg in Hitg.
+  apply Nat.le_antisymm in Hlab; [ | easy ].
+  subst b; clear Hitg.
+  rewrite Nat.sub_diag in Hab.
+  now rewrite g_aux_0_l in Hab.
+}
+apply IHitg with (itf := itf) in Hab; [ | easy | lia | lia ].
+rewrite Nat.gcd_comm in Hab.
+rewrite Nat.gcd_sub_diag_r in Hab; [ | easy ].
+rewrite Hab in Hab'.
+injection Hab'; clear Hab'; intros; subst.
+rewrite (Nat.gcd_comm b).
+progress f_equal.
+specialize (Nat.gcd_divide_l a b) as H1.
+specialize (Nat.gcd_divide_r a b) as H2.
+destruct H1 as (a', Ha).
+destruct H2 as (b', Hb).
+rewrite Hb at 1 3.
+rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_r in H ].
+rewrite Ha at 1 4.
+rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_r in H ].
+rewrite <- Nat.mul_sub_distr_r.
+rewrite Nat.div_mul; [ | now intros H; apply Nat.gcd_eq_0_r in H ].
+apply Nat.sub_add.
+apply (Nat.mul_le_mono_pos_r _ _ (Nat.gcd a b)); [ | lia ].
+apply Nat.neq_0_lt_0.
+now intros H; apply Nat.gcd_eq_0_r in H.
+Qed.
 
 Theorem f_eq_g_eq : ∀ n a b,
   f n = (a, b)
@@ -498,13 +621,13 @@ apply (f_aux_eq_g_aux_eq _ _ _ (S n)); [ lia | easy | easy ].
 Qed.
 
 Theorem g_eq_f_eq : ∀ n a b,
-  g (a, b) = n
+  b ≠ 0
+  → g (a, b) = n
   → f n = (a / Nat.gcd a b, b / Nat.gcd a b).
 Proof.
-intros * Hfab.
-... ...
-apply (g_aux_eq_f_aux_eq _ _ _ _ (max a b)); [ lia | easy | easy ].
-...
+intros * Hbz Hfab.
+now apply (g_aux_eq_f_aux_eq _ _ _ _ (max a b)); [ | lia | | ].
+Qed.
 
 Theorem f_g : ∀ n, g (f n) = n.
 Proof.
@@ -516,11 +639,19 @@ destruct ab as (a, b).
 now apply f_eq_g_eq.
 Qed.
 
-Theorem g_f : ∀ a b, f (g (a, b)) = (a / Nat.gcd a b, b / Nat.gcd a b).
+Theorem g_f :
+  ∀ a b,
+  f (g (a, b)) =
+    if Nat.eq_dec b 0 then (0, 1) else (a / Nat.gcd a b, b / Nat.gcd a b).
 Proof.
 intros.
+destruct (Nat.eq_dec b 0) as [Hbz| Hbz]. {
+  subst.
+  progress unfold g.
+  now rewrite g_aux_0_r.
+}
 progress unfold f, g.
 remember (g_aux (max a b) a b) as n eqn:Hn.
 symmetry in Hn.
-... ...
 now apply g_eq_f_eq.
+Qed.
